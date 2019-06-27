c70cfa45218133d040a83aae4a504d7b895f323d
zenladysh <52216064+zenladysh@users.noreply.github.com>
2019-06-27 11:50:57 +0300

    Task 1.3
    HomeWork2

---
+++ Task1_3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Task1_3 {
+	
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		System.out.print("Vvedite chislo:");
+		int num = in.nextInt();
+		
+		System.out.println("Vy vveli chislo: " + num);
+		in.close();
+	}
+}
+	
+	
