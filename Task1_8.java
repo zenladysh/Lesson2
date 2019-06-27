9d0f04039d71a9b02061429e8cc57153cdaf4687
zenladysh <52216064+zenladysh@users.noreply.github.com>
2019-06-27 11:51:34 +0300

    Task 1.8
    HomeWork2

---
+++ Task1_8.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Task1_8{
+	
+	public static void main(String[] args){
+	
+	Scanner in = new Scanner(System.in);
+	System.out.println("Vvedite chetyre chisla:");
+	int num1 = in.nextInt();
+	int num2 = in.nextInt();
+	int num3 = in.nextInt();
+	int num4 = in.nextInt();
+	
+	System.out.println("Vy vveli chisla: "+num1+" "+num2+" "+num3+" "+num4);
+	in.close();
+		
+	}
+}
