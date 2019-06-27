0f89b073f149cbead5cddec7df9e5f50b2847ff2
zenladysh <52216064+zenladysh@users.noreply.github.com>
2019-06-27 11:49:59 +0300

    Task 1_5
    HomeWork2

---
+++ Task1_5.java
@@ -0,0 +1,11 @@
+public class Task1_5 {
+	
+	public static void main(String[] args){
+		
+		int num1 = 1;
+		int num2 = 13;
+		int num3 = 49;
+		
+		System.out.println(num1+" "+num2+" "+num3);
+	}
+}
